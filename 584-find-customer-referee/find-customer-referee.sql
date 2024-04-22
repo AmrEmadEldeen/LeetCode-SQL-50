03be23e533a65773b92128eacad3c545b1fe1bf1
AmrEmadEldeen <85315099+AmrEmadEldeen@users.noreply.github.com>
2024-04-22 03:08:59 +0200

    Time: 995 ms (44.60%) | Memory: 0B (100.00%) - LeetSync

--- 584-find-customer-referee/find-customer-referee.sql
+++ 584-find-customer-referee/find-customer-referee.sql
@@ -1,4 +1,5 @@
 # Write your MySQL query statement below
 select name 
 from Customer
-where IFNULL(referee_id, 0) <> 2 ;
+where referee_id is null or referee_id <> 2 ;
+#where IFNULL(referee_id, 0) <> 2 ; // less performance
